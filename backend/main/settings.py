04a44e50d328a9a88be8ef023b610211b9aab0b8
Ivaylo <ivaylo.dzhedzhev@gmail.com>
2022-06-07 14:16:03 +0300

    Add Swagger autodocs

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "core",
 ]
 
@@ -123,3 +124,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Git Kiosk API",
+    "DESCRIPTION": "A system to store interesting Github projects",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+    "SCHEMA_PATH_PREFIX": r"/api/v[0-9]",
+}
--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
     path("api/v1/", include("core.urls")),
+    # Swagger docs
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("", lambda request: redirect("swagger-ui")),
 ]
