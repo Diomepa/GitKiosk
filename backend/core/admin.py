ba410ec791dfe6d860bb1203070409bab247f64a
Ivaylo <ivaylo.dzhedzhev@gmail.com>
2022-06-07 12:40:08 +0300

    Add ProjectEntry to admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,2 +1,12 @@
+from django.contrib import admin
+
+from core.models import ProjectEntry
 
 
+class ProjectEntryAdmin(admin.ModelAdmin):
+    list_display = ("name", "rating", "owner")
+    list_filter = ("rating", "owner")
+    search_fields = ("name", "description")
+
+
+admin.site.register(ProjectEntry, ProjectEntryAdmin)
